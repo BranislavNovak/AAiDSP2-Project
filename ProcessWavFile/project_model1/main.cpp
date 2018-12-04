96acef7b97505f68dfdcbef83daf9292df6f5656
student <student@domain.local>
2018-12-04 13:46:13 +0100

    Global variables and defines in common files!

--- ProcessWavFile/project_model1/main.cpp
+++ ProcessWavFile/project_model1/main.cpp
@@ -1,34 +1,13 @@
-
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <stdio.h>
 #include "WAVheader.h"
 #include "inverter.h"
+#include "processing.h"
 
-#define BLOCK_SIZE 16
-#define MAX_NUM_CHANNEL 8
-#define CHANNEL_NUMBER 5
-
-#define MINUS_TWO_DB 0.794328
-#define MINUS_SIX_DB 0.501187
-
+inverter_data_t inverter;
 double sampleBuffer[MAX_NUM_CHANNEL][BLOCK_SIZE];
-
-// Initialization constants
-//-------------------------------------------------
-const double initial_input_gain = 0.501187;			// (-6dB)
-const double initial_headroom_gain = 0.707946;		// (-3dB)
-//-------------------------------------------------
-
-// Global variables
-//-------------------------------------------------
-double leftTmpBuffer[BLOCK_SIZE];
-double rightTmpBuffer[BLOCK_SIZE];
-
-//-------------------------------------------------
-
-void processing(inverter_data_t* inverter);
 
 int main(int argc, char* argv[])
 {
@@ -37,8 +16,8 @@ int main(int argc, char* argv[])
 	char WavInputName[256];
 	char WavOutputName[256];
 	WAV_HEADER inputWAVhdr, outputWAVhdr;
-	inverter_data_t inverter;
-
+	
+	
 	// Init channel buffers
 	for (int i = 0; i<MAX_NUM_CHANNEL; i++)
 		memset(&sampleBuffer[i], 0, BLOCK_SIZE);
@@ -104,7 +83,7 @@ int main(int argc, char* argv[])
 				}
 			}
 
-			processing(&inverter);
+			processing();
 
 			for (int j = 0; j<BLOCK_SIZE; j++)
 			{
@@ -125,56 +104,4 @@ int main(int argc, char* argv[])
 	//-------------------------------------------------	
 
 	return 0;
-}
-
-void processing(inverter_data_t* inverter)
-{
-	// L  sampleBuffer[0]
-	// R  sampleBuffer[1]
-	// C  sampleBuffer[2]
-	// Ls sampleBuffer[3]
-	// Rs sampleBuffer[4]
-
-	double* ptr = NULL;
-	double* ptr_tmp_left = leftTmpBuffer;
-	double* ptr_tmp_right = rightTmpBuffer;
-
-	for (ptr = sampleBuffer[0]; ptr < sampleBuffer[0] + BLOCK_SIZE; ptr++)
-	{
-		// Passing left and right channel with -6dB gain and saving current state to tmp buffers.
-		*ptr *= initial_input_gain;							// L*(-6dB)
-		*ptr_tmp_left++ = *ptr;								// Saving state to LEFT tmp buffer
-
-		*(ptr + BLOCK_SIZE) *= initial_input_gain;					// R*(-6dB)
-		*ptr_tmp_right++ = *(ptr + BLOCK_SIZE);						// Saving state to RIGHT tmp buffer
-	}
-
-	for (ptr = sampleBuffer[0]; ptr < sampleBuffer[0] + BLOCK_SIZE; ptr++)
-	{
-		// Passing central channel as sum of L and R channel and -3dB gain
-		*((ptr + (2*BLOCK_SIZE))) = (*ptr + (*(ptr + BLOCK_SIZE)))*initial_headroom_gain;
-	}
-
-	// Passing left surround with inverter and -2dB gain
-	gst_audio_invert_transform(inverter, leftTmpBuffer, leftTmpBuffer, BLOCK_SIZE);
-
-	// Passing left surround with inverter and -2dB gain
-	gst_audio_invert_transform(inverter, rightTmpBuffer, rightTmpBuffer, BLOCK_SIZE);
-
-	// Reset tmp values
-	ptr_tmp_left = leftTmpBuffer;
-	ptr_tmp_right = rightTmpBuffer;
-
-	for (ptr = sampleBuffer[0]; ptr < sampleBuffer[0] + BLOCK_SIZE; ptr++)
-	{
-		// Passing left and right channel with last -6dB frin central channel
-		*ptr = *((ptr + (2 * BLOCK_SIZE))) * MINUS_SIX_DB;								// L = C*(-6dB)
-		*(ptr + BLOCK_SIZE) = *((ptr + (2 * BLOCK_SIZE))) * MINUS_SIX_DB;				// R = C*(-6dB)
-	
-		// Passing left surrond to original Ls buffer with -2dB gain plus left channel
-		*((ptr + (3 * BLOCK_SIZE))) = ((*ptr_tmp_left++) * MINUS_TWO_DB) + *ptr;
-
-		// Passing right surrond to original Rs buffer with -2dB gain plus right channel
-		*((ptr + (4 * BLOCK_SIZE))) = ((*ptr_tmp_right++) * MINUS_TWO_DB) + *(ptr + BLOCK_SIZE);
-	}
 }
